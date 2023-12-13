c8bb58e68000c90919212ac08c77b7cc5d46a130
gashok <113898567+ashokg1992@users.noreply.github.com>
2023-12-13 13:51:24 +0530

    Update QualityGateStatus.groovy

--- vars/QualityGateStatus.groovy
+++ vars/QualityGateStatus.groovy
@@ -1,6 +1,6 @@
 
 
-def call(credentialsId){
+def call(credentialsId){          #   to authenticate with sonar , we mention this credentil 
 
 waitForQualityGate abortPipeline: false, credentialsId: credentialsId
 
